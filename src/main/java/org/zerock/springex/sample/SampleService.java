caed45e0729b03ee97c668e01b197176c533bf60
ycraah <ycraah@gmail.com>
2024-09-16 13:54:50 +0900

    "어노테이션으로 서비스, 리포지토리 실행"

--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,4 +1,14 @@
 package org.zerock.springex.sample;
 
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@ToString
 public class SampleService {
+
+  @Autowired
+  private SampleDAO sampleDAO;
+
 }
