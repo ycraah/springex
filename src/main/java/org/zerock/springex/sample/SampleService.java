b743806c82c1c2ca493558c52584ca023e45b3ca
ycraah <ycraah@gmail.com>
2024-09-17 19:57:58 +0900

    "스프링 로그 기록 실행"

--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -3,12 +3,14 @@ package org.zerock.springex.sample;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 @Service
 @ToString
 @RequiredArgsConstructor
 public class SampleService {
 
+  @Qualifier("normal")
   private final SampleDAO sampleDAO;
 
 }
