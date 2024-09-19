4a305ca7468edf17bf0bb2e4a95827f35a1d562c
ycraah <ycraah@gmail.com>
2024-09-19 17:30:06 +0900

    "Formatter 이용 테스트"

--- src/main/java/org/zerock/springex/controller/SampleController.java
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+
 @Controller
 @Log4j2
 public class SampleController {
@@ -25,6 +27,12 @@ public class SampleController {
     log.info("ex2....");
     log.info("name: " + name);
     log.info("age: " + age);
+  }
+
+  @GetMapping("/ex3")
+  public void ex3(LocalDate dueDate){
+    log.info("ex3....");
+    log.info("dueDate: " + dueDate);
   }
 
 }
