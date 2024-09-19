9744fa3cd9c006f2d4ec4a5bb95b16a6f85547da
ycraah <ycraah@gmail.com>
2024-09-19 15:12:07 +0900

    "@Controller 사용 테스트"

---
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -0,0 +1,14 @@
+package org.zerock.springex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class SampleController {
+  @GetMapping("/hello")
+  public void hello() {
+    log.info("hello");
+  }
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ktf_y
+  Date: 2024-09-19
+  Time: 오후 3:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <H1>Hello JSP</H1>
+</body>
+</html>
